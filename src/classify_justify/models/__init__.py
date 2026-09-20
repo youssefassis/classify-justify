58d4979f7af2a34401437999bec69aab91f50baa
Youssef Assis <ysf.assis@gmail.com>
2026-09-20 20:27:05 +0200

    [FEAT] rebuild the classifier so a checkpoint loads without the dataset

---
+++ src/classify_justify/models/__init__.py
@@ -0,0 +1,15 @@
+from classify_justify.models.classifier import (
+    DefectClassifier,
+    ModelConfig,
+    build_model,
+    load_checkpoint,
+    save_checkpoint,
+)
+
+__all__ = [
+    "DefectClassifier",
+    "ModelConfig",
+    "build_model",
+    "load_checkpoint",
+    "save_checkpoint",
+]
